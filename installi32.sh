cd90e427b4b9fb3b72338ae0eb6cf548779c1cfd
drea8 <drea8an@yandex.com>
2019-06-08 06:39:43 -0500

    image directory org

--- installi32.sh
+++ installi32.sh
@@ -2,7 +2,10 @@ mkdir iso
 cd iso
 wget https://cdimage.debian.org/debian-cd/current/i386/iso-cd/debian-9.9.0-i386-netinst.iso
 cd ..
+mkdir img
+cd img
 qemu-img create debiani686.img 3G
-qemu-system-i386 -boot d -cdrom debian-9.9.0-i386-netinst.iso -hda debiani686.img
+cd ..
+qemu-system-i386 -boot d -cdrom /iso/debian-9.9.0-i386-netinst.iso -hda /img/debiani686.img
 # install to disk then run
-# qemu-system-i385 debiani686.img 
+# qemu-system-i386 debiani686.img 
