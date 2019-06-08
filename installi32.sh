c6e435a0c80ec34354342fdbf2bc602ab778aad2
drea8 <drea8an@yandex.com>
2019-06-08 07:12:18 -0500

    qemu doc

--- installi32.sh
+++ installi32.sh
@@ -9,3 +9,8 @@ cd ..
 qemu-system-i386 -boot d -cdrom /iso/debian-9.9.0-i386-netinst.iso -hda /img/debiani686.img
 # install to disk then run
 # qemu-system-i386 /img/debiani686.img 
+
+# see https://wiki.debian.org/QEMU#Networking
+# for virtual host network setup
+
+# further
